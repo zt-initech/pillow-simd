850d11b74e4f9297563f8934eca672cac36b7c9d
homm <homm86@gmail.com>
2016-07-04 02:18:11 +0300

    update version

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ except (ImportError, OSError):
     _tkinter = None
 
 NAME = 'Pillow-SIMD'
-PILLOW_VERSION = '3.3.0.post0'
+PILLOW_VERSION = '3.3.0.post1'
 JPEG_ROOT = None
 JPEG2K_ROOT = None
 ZLIB_ROOT = None
